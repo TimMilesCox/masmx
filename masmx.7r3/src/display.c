776ca5a07caa8ae35ffd708e68bf9e01ac99da7d
Tim Cox <TimMilesCox@gmx.ch>
2015-08-26 13:05:03 +0200

    add licence comments

--- masmx.7r3/src/display.c
+++ masmx.7r3/src/display.c
@@ -1,3 +1,27 @@
+
+/******************************************************************
+
+                Copyright Tim Cox, 2015
+
+                TimMilesCox@gmx.ch
+
+        This source code is part of the masmx.7r3
+        target-independent meta-assembler
+
+        The masmx.7r3 meta-assembler is free software licensed
+        with the GNU General Public Licence Version 3
+
+        The same licence encompasses all accompanying software
+        and documentation
+
+        The full licence text is included with these materials
+
+        See also the licensing notice at the foot of this document
+
+*******************************************************************/
+
+
+
 static void print_item(line_item *item)
 {
    int			 x = RADIX/8;
@@ -704,3 +728,34 @@ static void summarise_revision(location_counter *q, unsigned long v, unsigned lo
    else
    printf(": $(%2.2X) :%0*lX:%0*lX\n", q - locator, apw, v, apw, high);
 }
+
+/**************************************************************************
+
+
+LICENCE NOTE
+
+    Copyright Tim Cox, 2015
+    TimMilesCox@gmx.ch
+
+    This source code is part of the masmx.7r3 target-independent
+    meta-assembler.
+
+    masmx.7r3 is free software. It is licensed
+    under the GNU General Public Licence Version 3.
+
+    You can redistribute it and/or modify masmx.7r3
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    masmx.7r3 is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with masm.7r3.  If not, see &lt;http://www.gnu.org/licenses/&gt;.
+
+*************************************************************************/
+
+
--- masmx.7r3/src/ltrailer.c
+++ masmx.7r3/src/ltrailer.c
@@ -1,3 +1,28 @@
+
+/******************************************************************
+
+                Copyright Tim Cox, 2015
+
+                TimMilesCox@gmx.ch
+
+        This source code is part of the masmx.7r3
+        target-independent meta-assembler
+
+        The masmx.7r3 meta-assembler is free software licensed
+        with the GNU General Public Licence Version 3
+
+        The same licence encompasses all accompanying software
+        and documentation
+
+        The full licence text is included with these materials
+
+        See also the licensing notice at the foot of this document
+
+*******************************************************************/
+
+
+
+
 #ifdef STRUCTURE_DEPTH
 
 static void load_trailer(char *s, char *limit)
@@ -49,3 +74,34 @@ static void load_trailer(char *s, char *limit)
 }
 
 #endif	/*	STRUCTURE_DEPTH	*/
+
+/**************************************************************************
+
+
+LICENCE NOTE
+
+    Copyright Tim Cox, 2015
+    TimMilesCox@gmx.ch
+
+    This source code is part of the masmx.7r3 target-independent
+    meta-assembler.
+
+    masmx.7r3 is free software. It is licensed
+    under the GNU General Public Licence Version 3.
+
+    You can redistribute it and/or modify masmx.7r3
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    masmx.7r3 is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with masm.7r3.  If not, see &lt;http://www.gnu.org/licenses/&gt;.
+
+*************************************************************************/
+
+
