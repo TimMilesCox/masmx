2d3733be6c458c4890e2dd90159a033f478a81c9
Tim Cox <TimMilesCox@gmx.ch>
2015-09-05 00:58:18 +0200

    set a clear example

--- masmx.7r3/src/csynonym.c
+++ masmx.7r3/src/csynonym.c
@@ -71,7 +71,7 @@ LICENCE NOTE
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with masm.7r3.  If not, see &lt;http://www.gnu.org/licenses/&gt;.
+    along with masmx.7r3.  If not, see <http://www.gnu.org/licenses/>.
 
 *************************************************************************/
 
